bd1d66d76d22cbebfdb4da41d9f49e9bc23a4a89
Rulep15 <lucasrulep@gmail.com>
2023-01-20 18:16:22 -0300

    README

---
+++ Codigo.js
@@ -0,0 +1,21 @@
+//TIPO DE DATOS EN JAVASCRIPT
+/*
+String = "cadena de texto"
+Number = 19
+Boolean = false
+*/
+//DECLARACIONES Y LOS TIPO DE ALCANZE-SCOPE
+/*
+var = Nos ejecuta todo 
+let = Nos limita la ejecucion hasta el bloque que ejecutamos
+const = Siempre va a ser la misma variable por que son variables constantes y nunca van a poder a cambiar si la ya decidimos antes
+*/
+//ASI SE DECLARA UNA VARIABLE
+/*let numero;
+numero = 19;
+alert(numero);*/
+/*const numero = 2;
+alert(numero)*/
+
+
+
---
+++ Prueba SQL.sql
@@ -0,0 +1,5 @@
+CREATE TABLE prueba1(
+    id_prueba integer;
+    descripcion varchar;
+    PRIMARY KEY id_prueba;
+);
